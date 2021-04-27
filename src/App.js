beaba8759b6705c221aa7f2b5f1ec4758eb13edf
Julaam Diop <jeebe.diop@gmail.com>
2021-04-27 13:29:31 -0500

    updating app with shop message, collection preview and item components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component.jsx'
+
 import './pages/homepage/homepage.styles.scss'
 import './App.css';
 
 import {Route, Switch, Link} from 'react-router-dom';
 
-const HatsPage = () => (
-  <div>
-    <h1> HATS PAGE </h1>
-  </div>
-)
-
-
-function App(props) {
+function App() {
   return (
     <div>
     <Link to='/'> Home </Link>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
     // <HomePage />
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import SHOP_DATA from './shop.data.js';
+import PreviewCollection from '../../components/preview-collection/preview-collection.component.jsx'
+
+// creating a class component since we are storing data and states
+class ShopPage extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+
+  render() {
+    const {collections} = this.state;
+    return (<div className='shop-page'>
+      {
+          collections.map(({id, ...otherCollectionProps}) => (
+            <PreviewCollection key={id} {...otherCollectionProps} />
+          ))
+      }
+      </div>);
+  }
+}
+
+export default ShopPage;
