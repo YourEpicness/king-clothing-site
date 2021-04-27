e454f7fb51543937cb8d8fe53d8e06069753e5c9
Julaam Diop <jeebe.diop@gmail.com>
2021-04-27 12:43:08 -0500

    updated menu components to add navigation withRouter HOC

--- src/App.js
+++ src/App.js
@@ -3,9 +3,25 @@ import HomePage from './pages/homepage/homepage.component'
 import './pages/homepage/homepage.styles.scss'
 import './App.css';
 
-function App() {
+import {Route, Switch, Link} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+)
+
+
+function App(props) {
   return (
-    <HomePage />
+    <div>
+    <Link to='/'> Home </Link>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
+    // <HomePage />
   );
 }
 
