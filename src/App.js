998eed1976e1ac57fab36e54edd0630c2a9bcd12
Julaam Diop <jeebe.diop@gmail.com>
2021-04-27 18:58:22 -0500

    added in sign up, sign in, and form components as well as signup page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component.jsx'
 import Header from './components/header/header.component.jsx';
+import SignInandSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
 
 import './pages/homepage/homepage.styles.scss'
 import './App.css';
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInandSignUpPage} />
       </Switch>
     </div>
     // <HomePage />
