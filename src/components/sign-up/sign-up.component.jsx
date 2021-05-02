763dceff46d822e683169c3df2d5b10abf661242
Julaam Diop <jeebe.diop@gmail.com>
2021-05-01 19:28:04 -0500

    finishing sign in and auth

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -70,7 +70,7 @@ class SignUp extends React.Component {
           name='email'
           value={email}
           onChange={this.handleChange}
-          label='Display Name'
+          label='Email'
           required
         />
 
